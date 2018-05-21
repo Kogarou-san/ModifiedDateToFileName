1a5fbbbc433e5345c6028e8783d51dbafe1a3e68
Pedro Lourenço <madmilkpt@gmail.com>
2018-05-21 12:01:27 +0100

    Final edit

--- ModifiedDateToFileName/FormMain.Designer.cs
+++ ModifiedDateToFileName/FormMain.Designer.cs
@@ -28,19 +28,19 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
+			this.buttonOpen = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// button1
+			// buttonOpen
 			// 
-			this.button1.Location = new System.Drawing.Point(90, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "Open Folder";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.buttonOpen.Location = new System.Drawing.Point(90, 12);
+			this.buttonOpen.Name = "buttonOpen";
+			this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+			this.buttonOpen.TabIndex = 0;
+			this.buttonOpen.Text = "Open Folder";
+			this.buttonOpen.UseVisualStyleBackColor = true;
+			this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
 			// 
 			// label1
 			// 
@@ -57,7 +57,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(246, 45);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.buttonOpen);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Name = "FormMain";
 			this.Text = "ModifiedDateToFileName";
@@ -68,7 +68,7 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button buttonOpen;
 		private System.Windows.Forms.Label label1;
 	}
 }
--- ModifiedDateToFileName/FormMain.cs
+++ ModifiedDateToFileName/FormMain.cs
@@ -15,7 +15,7 @@ namespace ModifiedDateToFileName
 		String directory;
 		Stopwatch stopwatch = new Stopwatch();
 
-		private void button1_Click(object sender, EventArgs e)
+		private void buttonOpen_Click(object sender, EventArgs e)
 		{
 			OpenFileDialog openFileDialog = new OpenFileDialog
 			{
@@ -34,26 +34,23 @@ namespace ModifiedDateToFileName
 				foreach (String fileName in fileEntries)
 				{
 					String oldName, extension;
-					
 					oldName = Path.GetFileName(fileName);
 					extension = Path.GetExtension(openFileDialog.FileName);
 
 					DateTime dateTime = File.GetLastWriteTime(fileName);
 
 					File.AppendAllText("info.txt", String.Concat("DST for " + oldName + " is " + TimeZoneInfo.Local.IsDaylightSavingTime(File.GetLastWriteTime(fileName)).ToString(), '.'));
-					//MessageBox.Show("DST for " + name + " is " + TimeZoneInfo.Local.IsDaylightSavingTime(File.GetLastWriteTime(fileName)).ToString(), "Timezone", MessageBoxButtons.OK);
-					
+
 					if (TimeZoneInfo.Local.IsDaylightSavingTime(File.GetLastWriteTime(fileName)))
 						dateTime.AddHours(1);
 					String newName = String.Concat(dateTime.ToString("yyyy-MM-dd hh.mm.ss"), extension);
 
 					File.Move(String.Concat(path, '\\', oldName),
 							String.Concat(path, '\\', newName));
-					File.AppendAllText("info.txt", String.Concat(" Renamed ", oldName, " to ", newName, Environment.NewLine)); 
-					i++;
+					File.AppendAllText("info.txt", String.Concat(" Renamed ", oldName, " to ", newName, Environment.NewLine));
 				}
 				stopwatch.Stop();
-				String message = String.Concat("\n\n", i - 1, " out of ", fileEntries.Length, " files renamed succ-essfully in ",
+				String message = String.Concat("\n\n", i - 1, " out of ", fileEntries.Length, " files renamed successfully in ",
 												(float)stopwatch.ElapsedMilliseconds / 1000, " seconds.");
 				File.AppendAllText("info.txt", message + Environment.NewLine);
 				MessageBox.Show(message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
